b7772b0b742856325fd1693d3c0cf079af9c222e
Lea Berthomier <lea.berthomier@meteo.fr>
2026-02-05 15:35:41 +0000

    compute stats

--- cams/settings.py
+++ cams/settings.py
@@ -2,3 +2,4 @@ from pathlib import Path
 
 CAMS_DATA_DIR = Path("/scratch/shared/cams/")
 CAMS_DATASET = CAMS_DATA_DIR / "o3_15h_SOL/"
+STATS_PATH = Path("cams/species_stats.json")
---
+++ scripts/compute_stats.py
@@ -0,0 +1,28 @@
+from cams.settings import CAMS_DATASET, STATS_PATH
+import datetime as dt
+from cams.sample import Sample
+import numpy as np
+from tqdm import tqdm
+import json
+
+
+# Get list of samples
+list_runs = sorted(list(CAMS_DATASET.glob("input/*.netcdf")))
+list_dates = [dt.datetime.strptime(path.stem, "%Y_%m_%d") for path in list_runs]
+list_samples = [Sample(date_run, 15) for date_run in list_dates]
+list_samples = [sample for sample in list_samples if sample.is_valid]
+
+vmin, vmax = np.inf, - np.inf  # Init min and max
+
+for sample in tqdm(list_samples, desc="Computing statistics"):
+    data = sample.target_data.tensor[0].numpy()
+    new_min, new_max = np.min(data), np.max(data)
+    vmin = min(vmin, new_min)
+    vmax = max(vmax, new_max)
+
+stats = {"O3": {"min": float(vmin), "max": float(vmax)}}
+print('stats : ', stats)
+
+# Save stats as json
+with open(STATS_PATH, "w") as f:
+    json.dump(stats, f, indent=4)

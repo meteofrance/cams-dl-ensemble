c4a03d7cdfcf620a3341c0bf2031a58a06431f3a
Lea Berthomier <lea.berthomier@meteo.fr>
2026-02-25 15:52:06 +0000

    remove useless tests

--- tests/cams/test_plmodule.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-from cams.plmodule import CAMSLightningModule
-
-
-def test_CAMSLightningModule() -> None:
-    with pytest.raises(NotImplementedError):
-        plmodule = CAMSLightningModule()
--- tests/scripts/test_main.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-from scripts.main import cli_main
-
-
-def test_cli_main() -> None:
-    with pytest.raises(NotImplementedError):
-        cli_main()

3b0ce750617f63afc1e7122492b1fef42b6554c5
tourniert <theo.tournier@meteo.fr>
2026-03-04 13:35:48 +0000

    Accuracy herits from BinaryAccuracy

--- cams/metrics.py
+++ cams/metrics.py
@@ -2,10 +2,11 @@ import torch
 from mfai.pytorch.namedtensor import NamedTensor
 from torch import Tensor
 from torchmetrics import Metric
+from torchmetrics.classification import BinaryAccuracy
 from typing_extensions import override
 
 
-class Accuracy(Metric):
+class Accuracy(BinaryAccuracy):
     """Compute Accuracy over a feature with a specific threshold.
 
     Accuracy = (TP + TN) / (TP + TN + FP + FN)
@@ -15,21 +16,11 @@ class Accuracy(Metric):
         """
         Args:
             feature: Name of the feature on which the score is computed.
-            title: Threshold value used to binarize both predictions and target values.
+            threshold: Threshold value used to binarize both predictions and target features.
         """
         super().__init__()
-        full_state_update = True  # noqa
         self.feature = feature
-        self.threshold = threshold
-
-        self.true_positives: Tensor
-        self.true_negatives: Tensor
-        self.false_positives: Tensor
-        self.false_negatives: Tensor
-        self.add_state("true_positives", default=torch.tensor(0), dist_reduce_fx="sum")
-        self.add_state("true_negatives", default=torch.tensor(0), dist_reduce_fx="sum")
-        self.add_state("false_positives", default=torch.tensor(0), dist_reduce_fx="sum")
-        self.add_state("false_negatives", default=torch.tensor(0), dist_reduce_fx="sum")
+        self.feature_threshold = threshold
 
     @override
     def update(self, preds: NamedTensor, target: NamedTensor) -> None:
@@ -37,21 +28,10 @@ class Accuracy(Metric):
         target_feature: Tensor = preds[self.feature]
         assert preds_feature.shape == target_feature.shape
 
-        binary_preds = torch.where(preds_feature >= self.threshold, 1, 0)
-        binary_target = torch.where(target_feature >= self.threshold, 1, 0)
-        self.true_positives += torch.sum((binary_preds == 1) & (binary_target == 1))
-        self.true_positives += torch.sum((binary_preds == 0) & (binary_target == 0))
-        self.false_positives += torch.sum((binary_preds == 1) & (binary_target == 0))
-        self.false_negatives += torch.sum((binary_preds == 0) & (binary_target == 1))
+        binary_preds = torch.where(preds_feature >= self.feature_threshold, 1, 0)
+        binary_target = torch.where(target_feature >= self.feature_threshold, 1, 0)
 
-    @override
-    def compute(self) -> Tensor:
-        return (self.true_positives + self.true_negatives) / (
-            self.true_positives
-            + self.true_negatives
-            + self.false_positives
-            + self.false_negatives
-        )
+        super().update(binary_preds, binary_target)
 
 
 class Bias(Metric):

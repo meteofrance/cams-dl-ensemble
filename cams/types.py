5d672166e8262e8783e2b09dfbece86f860786d4
Lea Berthomier <lea.berthomier@meteo.fr>
2026-03-04 12:38:31 +0000

    add types for statistics

---
+++ cams/types.py
@@ -0,0 +1,23 @@
+from typing import Literal
+
+STATISTICS_NAMES = (
+    "mean",
+    "amin",
+    "argmin",
+    "amax",
+    "argmax",
+    "median",
+    "skew",
+    "kurtosis",
+)
+
+StatisticsNames = Literal[
+    "mean",
+    "amin",
+    "argmin",
+    "amax",
+    "argmax",
+    "median",
+    "skew",
+    "kurtosis",
+]

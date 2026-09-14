9ad4406ea37e94dbf20ecfd8b3a2313615320d1e
Oscar Dewasmes <oscar.dewasmes@gmail.com>
2026-09-14 14:37:00 +0000

    Use typing_extensions.override in file_count_calendar for 3.12 compat

--- scripts/calendars/file_count_calendar.py
+++ scripts/calendars/file_count_calendar.py
@@ -1,9 +1,9 @@
 import datetime as dt
 from pathlib import Path
-from typing import override
 
 from calendardataviz import InspectorABC, RichString, start_app
 from calendardataviz.colors import RDYLGN, color_from_pct
+from typing_extensions import override
 
 UNDER_0_COLOR = RichString("X", "#77CBFF", "#cb31ff")
 OVER_1_COLOR = RichString("X", "#FF003C", "#e9a7ff")
